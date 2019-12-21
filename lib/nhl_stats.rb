640dc4d4e892e6065f82c44e9792458e521c19ff
Brian Nicholls <nichol88@gmail.com>
2019-12-20 21:20:24 -0500

    fixing dependencies

--- lib/nhl_stats.rb
+++ lib/nhl_stats.rb
@@ -1,7 +1,8 @@
   require 'pry'
   require 'open-uri'
   require 'nokogiri'
-  require_relative "./nhl_stats/player"
   require_relative "./nhl_stats/version"
+
   require_relative "./nhl_stats/cli"
-  require_relative "./nhl_stats/scraper"
+  require_relative "./nhl_stats/player"
+    require_relative "./nhl_stats/scraper"
